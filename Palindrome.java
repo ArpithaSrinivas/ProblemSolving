f159167f5ef990207115a29da20f1160ffaeccb7
ArpithaSrinivas <42426791+ArpithaSrinivas@users.noreply.github.com>
2021-03-30 22:44:11 -0700

    To find if a number is a palindrome

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args){
+       int count=0 , orginal_number=4554;
+       int copiedNumber= orginal_number;
+
+       while(copiedNumber!=0) {
+           int digit = copiedNumber % 10; 4554
+           count = count * 10 + digit; 4554
+           copiedNumber = copiedNumber / 10; 455,45,4
+       }
+           if (orginal_number==count) {
+               System.out.println("Entered number is palindrome");
+           }
+           else {
+               System.out.println(" Entered number is not a palindrome");
+           }
