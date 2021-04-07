b49b9692d60f38fee6fd1880f24e3a99699a25ac
ArpithaSrinivas <42426791+ArpithaSrinivas@users.noreply.github.com>
2021-04-06 18:15:13 -0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,9 @@
+/*Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+Author: @arpithasrinivas
+*/
+
 public class PalindromeNumber {
 
     public static void main(String[] args){
